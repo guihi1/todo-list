a7383f2d21bb561505e9f9a715cc74e9eb8ac93a
guihi1 <gui.hikio@gmail.com>
2022-09-20 14:21:12 -0300

    Make google icon change on click

--- src/UI.js
+++ src/UI.js
@@ -1,11 +1,14 @@
 export function dropdown() {
     const dropButton = document.querySelector(".dropdown-button");
+    const expand = document.getElementById("expand");
     dropButton.addEventListener("click", () => {
         const dropContent = document.querySelector(".dropdown-content");
         if(dropContent.classList.contains("show")) {
             dropContent.classList.remove("show");
+            expand.textContent = "expand_more";
         } else {
             dropContent.classList.add("show");
+            expand.textContent = "expand_less";
         }
     });   
 }
