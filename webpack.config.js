5597a18a7758d164eca9d1256155231580120fdf
guihi1 <gui.hikio@gmail.com>
2022-09-22 13:04:33 -0300

    Remove some loader that won't be used

--- webpack.config.js
+++ webpack.config.js
@@ -13,15 +13,6 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
     ],
   },
 };
-Footer
