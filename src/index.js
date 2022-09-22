9d43f424f629e0aa9046b5414d515e9efa91f2ed
guihi1 <gui.hikio@gmail.com>
2022-09-22 13:00:58 -0300

    Add event listener to form button

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 import "./styles.css";
-import { dropdown } from "./UI";
+import { dropdown, addProject, showForm, hideForm, showTasks } from "./UI";
+import { task } from "./constructor";
 
 dropdown();
+addProject();
+showForm();
+hideForm();
+
+export let myTasks = [];
+
+const addButton = document.getElementById("add");
+addButton.addEventListener("click", () => {
+    let title = document.getElementById("title").value;
+    let date = document.getElementById("date").value;
+    let priority = document.getElementById("priority").value;
+    let description = document.getElementById("description").value;
+    const newTask = new task(title, date, priority, description);
+    myTasks.push(newTask);
+    document.getElementById("title").value = "";
+    document.getElementById("date").value = "";
+    document.getElementById("description").value = "";
+    showTasks();
+})
