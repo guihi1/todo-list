58636e6c704e3c8fd16bd6903d5d62b4a4ae6744
guihi1 <gui.hikio@gmail.com>
2022-09-22 18:52:03 -0300

    Add an if for edits to a task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./styles.css";
-import { dropdown, addProject, showForm, hideForm, showTasks } from "./UI";
+import { dropdown, addProject, showForm, hideForm, showTasks, index } from "./UI";
 import { task } from "./constructor";
 
 dropdown();
@@ -11,14 +11,31 @@ export let myTasks = [];
 
 const addButton = document.getElementById("add");
 addButton.addEventListener("click", () => {
-    let title = document.getElementById("title").value;
-    let date = document.getElementById("date").value;
-    let priority = document.getElementById("priority").value;
-    let description = document.getElementById("description").value;
-    const newTask = new task(title, date, priority, description);
-    myTasks.push(newTask);
-    document.getElementById("title").value = "";
-    document.getElementById("date").value = "";
-    document.getElementById("description").value = "";
-    showTasks();
+    if(document.querySelector("form").classList.contains("editing")) {
+        let title = document.getElementById("title").value;
+        let date = document.getElementById("date").value;
+        let priority = document.getElementById("priority").value;
+        let description = document.getElementById("description").value;
+        myTasks[index] = new task(title, date, priority, description);
+        document.querySelector("form").classList.remove("editing");
+        document.getElementById("add").textContent = "Add task";
+        document.getElementById("title").value = "";
+        document.getElementById("date").value = "";
+        document.getElementById("description").value = "";
+        document.getElementById("priority").value = "1";
+        showTasks();
+    } else {
+        let title = document.getElementById("title").value;
+        let date = document.getElementById("date").value;
+        let priority = document.getElementById("priority").value;
+        let description = document.getElementById("description").value;
+        const newTask = new task(title, date, priority, description);
+        myTasks.push(newTask);
+        document.getElementById("title").value = "";
+        document.getElementById("date").value = "";
+        document.getElementById("description").value = "";
+        document.getElementById("priority").value = "1";
+        showTasks();
+    }
 })
+
