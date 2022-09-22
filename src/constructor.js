0db1cd9508b0ff63b472d6b8c523116fbc89b78e
guihi1 <gui.hikio@gmail.com>
2022-09-22 13:01:52 -0300

    Create constructor

---
+++ src/constructor.js
@@ -0,0 +1,8 @@
+export class task {
+    constructor(title, date, priority, description ){
+        this.title = title;
+        this.priority = priority;
+        this.description = description;
+        this.date = date;
+    }
+}
