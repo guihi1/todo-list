d9dd408cbf48cbbb52b6a701235c689dcaa131d8
guihi1 <gui.hikio@gmail.com>
2022-09-23 10:37:35 -0300

    Add project

--- src/constructor.js
+++ src/constructor.js
@@ -1,8 +1,9 @@
 export class task {
-    constructor(title, date, priority, description ){
+    constructor(title, date, priority, description, project){
         this.title = title;
         this.priority = priority;
         this.description = description;
         this.date = date;
+        this.project = project;
     }
 }
