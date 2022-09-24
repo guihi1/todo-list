53da82eaad1e3e3cd1cfd97ea17fac4f074554a3
guihi1 <gui.hikio@gmail.com>
2022-09-24 20:02:42 -0300

    Add function showProjectTasks

--- src/project.js
+++ src/project.js
@@ -1,3 +1,6 @@
+import { showTasks } from "./UI";
+import { myTasks } from ".";
+
 export function newProject(){
     const button = document.querySelector("#projectName-button");
     button.addEventListener("click", () => {
@@ -11,6 +14,30 @@ export function newProject(){
     })
 }
 
-export function removeProject() {
-    
+export let today = new Date();
+let dd = String(today.getDate()).padStart(2, '0');
+let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+let yyyy = today.getFullYear();
+today = yyyy + '-' + mm + '-' + dd;
+
+export function showProjectTasks() {
+    const inbox = document.querySelector("#inbox");
+    inbox.addEventListener("click", () => {
+        document.querySelector("#content-title").textContent = "Inbox";
+        let inboxTasks = myTasks.filter(x => x.project === "inbox");
+        showTasks(inboxTasks);
+    })
+
+    const todayProject = document.querySelector("#today");
+    todayProject.addEventListener("click", () => {
+        document.querySelector("#content-title").textContent = "Today";
+        let todayTasks = myTasks.filter(x => x.date.substring(0, 10) === today);
+        showTasks(todayTasks);
+    })
+
+    const allTasks = document.querySelector("#all-tasks");
+    allTasks.addEventListener("click", () => {
+        document.querySelector("#content-title").textContent = "All tasks";
+        showTasks(myTasks);
+    })
 }
