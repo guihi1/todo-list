02ea190afba29b095e349cea7f50697689bf34f1
guihi1 <gui.hikio@gmail.com>
2022-09-25 20:04:11 -0300

    Add option to form if project exists in storage

--- src/project.js
+++ src/project.js
@@ -2,6 +2,25 @@ import { showTasks } from "./UI";
 import { myTasks } from ".";
 
 export function newProject(){
+    let projectNameArray = [];
+    for(const task of myTasks){
+        if(task.project !== "inbox"){
+            projectNameArray.push(task.project);
+        }
+    }
+
+    let uniqueProjectName = Array.from(new Set(projectNameArray));
+
+    for(let i = 0; i < uniqueProjectName.length; i++) {
+        const projectSelect = document.getElementById("project");
+        let projectName = uniqueProjectName[i];
+        const newOption = document.createElement("option");
+        newOption.textContent = projectName;
+        newOption.setAttribute("value", `${projectName}`);
+        projectSelect.appendChild(newOption);
+        document.getElementById("new-project").value = "";
+    }
+    
     const button = document.querySelector("#projectName-button");
     button.addEventListener("click", () => {
         const projectSelect = document.getElementById("project");
