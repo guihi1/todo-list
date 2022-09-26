15f2e414b3929458fc980de9d055c22a5ca9bb98
guihi1 <gui.hikio@gmail.com>
2022-09-26 17:00:54 -0300

    Linting

--- src/project.js
+++ src/project.js
@@ -1,62 +1,62 @@
-import { showTasks } from "./UI";
-import { myTasks } from ".";
+import { showTasks } from './UI';
+import { myTasks } from '.';
 
-export function newProject(){
-    let projectNameArray = [];
-    for(const task of myTasks){
-        if(task.project !== "inbox"){
-            projectNameArray.push(task.project);
-        }
+export function newProject() {
+  const projectNameArray = [];
+  for (const task of myTasks) {
+    if (task.project !== 'inbox') {
+      projectNameArray.push(task.project);
     }
+  }
 
-    let uniqueProjectName = Array.from(new Set(projectNameArray));
+  const uniqueProjectName = Array.from(new Set(projectNameArray));
 
-    for(let i = 0; i < uniqueProjectName.length; i++) {
-        const projectSelect = document.getElementById("project");
-        let projectName = uniqueProjectName[i];
-        const newOption = document.createElement("option");
-        newOption.textContent = projectName;
-        newOption.setAttribute("value", `${projectName}`);
-        projectSelect.appendChild(newOption);
-        document.getElementById("new-project").value = "";
-    }
-    
-    const button = document.querySelector("#projectName-button");
-    button.addEventListener("click", () => {
-        const projectSelect = document.getElementById("project");
-        let projectName = document.getElementById("new-project").value;
-        const newOption = document.createElement("option");
-        newOption.textContent = projectName;
-        newOption.setAttribute("value", `${projectName}`);
-        projectSelect.appendChild(newOption);
-        document.getElementById("new-project").value = "";
-    })
+  for (let i = 0; i < uniqueProjectName.length; i += 1) {
+    const projectSelect = document.getElementById('project');
+    const projectName = uniqueProjectName[i];
+    const newOption = document.createElement('option');
+    newOption.textContent = projectName;
+    newOption.setAttribute('value', `${projectName}`);
+    projectSelect.appendChild(newOption);
+    document.getElementById('new-project').value = '';
+  }
+
+  const button = document.querySelector('#projectName-button');
+  button.addEventListener('click', () => {
+    const projectSelect = document.getElementById('project');
+    const projectName = document.getElementById('new-project').value;
+    const newOption = document.createElement('option');
+    newOption.textContent = projectName;
+    newOption.setAttribute('value', `${projectName}`);
+    projectSelect.appendChild(newOption);
+    document.getElementById('new-project').value = '';
+  });
 }
 
 export let today = new Date();
-let dd = String(today.getDate()).padStart(2, '0');
-let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-let yyyy = today.getFullYear();
-today = yyyy + '-' + mm + '-' + dd;
+const dd = String(today.getDate()).padStart(2, '0');
+const mm = String(today.getMonth() + 1).padStart(2, '0'); // January is 0!
+const yyyy = today.getFullYear();
+today = `${yyyy}-${mm}-${dd}`;
 
 export function showProjectTasks() {
-    const inbox = document.querySelector("#inbox");
-    inbox.addEventListener("click", () => {
-        document.querySelector("#content-title").textContent = "Inbox";
-        let inboxTasks = myTasks.filter(x => x.project === "inbox");
-        showTasks(inboxTasks);
-    })
+  const inbox = document.querySelector('#inbox');
+  inbox.addEventListener('click', () => {
+    document.querySelector('#content-title').textContent = 'Inbox';
+    const inboxTasks = myTasks.filter((x) => x.project === 'inbox');
+    showTasks(inboxTasks);
+  });
 
-    const todayProject = document.querySelector("#today");
-    todayProject.addEventListener("click", () => {
-        document.querySelector("#content-title").textContent = "Today";
-        let todayTasks = myTasks.filter(x => x.date.substring(0, 10) === today);
-        showTasks(todayTasks);
-    })
+  const todayProject = document.querySelector('#today');
+  todayProject.addEventListener('click', () => {
+    document.querySelector('#content-title').textContent = 'Today';
+    const todayTasks = myTasks.filter((x) => x.date.substring(0, 10) === today);
+    showTasks(todayTasks);
+  });
 
-    const allTasks = document.querySelector("#all-tasks");
-    allTasks.addEventListener("click", () => {
-        document.querySelector("#content-title").textContent = "All tasks";
-        showTasks(myTasks);
-    })
+  const allTasks = document.querySelector('#all-tasks');
+  allTasks.addEventListener('click', () => {
+    document.querySelector('#content-title').textContent = 'All tasks';
+    showTasks(myTasks);
+  });
 }
